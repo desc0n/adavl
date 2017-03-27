3ba5435a2424b9f1d8bb801c4cbc2d3a2326a186
desc0n <desconbk@yandex.ru>
2017-03-27 22:47:09 +1000

    Добавляю админские стили и шрифты

---
+++ assets/admin/js/scripts.js
@@ -0,0 +1,40 @@
+$(document).ready(function() {
+    $('#street').typeahead({
+        source: function (query, process) {
+            return $.get('/ajax/find_street', {query: query}, function (data) {
+                var json = JSON.parse(data); // string to json
+
+                return process(json);
+            });
+        }
+    });
+});
+
+function redactPortfolioItemImg(id, src)
+{
+    $('#redactImgModal .modal-body')
+        .html('')
+        .append('<img src="/public/img/thumb/' + src + '" data-id="' + id + '">')
+    ;
+
+    $('#redactImgModal').modal('toggle');
+}
+
+function removePortfolioItemImg()
+{
+    var id = $('#redactImgModal .modal-body img').data('id');
+
+    $.ajax({url: '/ajax/remove_portfolio_item_img', type: 'POST', data: {id: id}, async: true})
+        .done(function () {
+            $('#redactImgModal').modal('toggle');
+            $('#portfolioItemImg' + id).remove();
+        });
+}
+
+function removePortfolioItem(id)
+{
+    $.ajax({url: '/ajax/remove_portfolio_item', type: 'POST', data: {id: id}, async: true})
+        .done(function () {
+            $('#portfolioItemRow' + id).remove();
+        });
+}
