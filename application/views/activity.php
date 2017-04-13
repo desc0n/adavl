041ce6c3452cee31415655e604269561c2d06551
desc0n <desconbk@yandex.ru>
2017-04-13 22:34:19 +1000

    Добавляю код google analytics

--- application/views/activity.php
+++ application/views/activity.php
@@ -18,3 +18,13 @@ $contentModel = Model::factory('Content');
         <img class="contactImage" src="/public/images/activity_img.jpg"/>
     </article>
 </div>
+<script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-97326796-1', 'auto');
+    ga('send', 'pageview');
+
+</script>
